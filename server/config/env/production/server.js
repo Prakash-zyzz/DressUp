3d70f831cfe4a5579fa015ad063c4d209412990b
Prakash-zyzz <115093069+Prakash-zyzz@users.noreply.github.com>
2024-02-11 10:15:40 +0530

    Update server.js

--- server/config/env/production/server.js
+++ server/config/env/production/server.js
@@ -1,8 +1,6 @@
 module.exports = ({ env }) => ({
-    url: env("https://dressup-8pk8.onrender.com/"),
-    database: {
-        connection: {
-            filename: 'D:/sqlite_data/data.db', // Adjust the path and database name as needed
-        },
-    },
+  url: env("https://dressup-8pk8.onrender.com/"),
+  dirs: {
+    public: "/data/public"
+  },
 });
