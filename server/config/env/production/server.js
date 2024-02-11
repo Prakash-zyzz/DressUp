02b9c8b6b58cd9eb6f35ee3a28d24ca83724f1db
Prakash-zyzz <115093069+Prakash-zyzz@users.noreply.github.com>
2024-02-11 17:26:09 +0530

    Delete server/config/env/production directory

--- server/config/env/production/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-    url: env("https://dress-ri6r.onrender.com/"),
-    database: {
-        connection: {
-            filename:  "D:/data/public", // Adjust the path and database name as needed
-        },
-    },
