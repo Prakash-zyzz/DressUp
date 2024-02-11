f2c411536f99a3cbf29dba895b3628067d1a5477
Prakash-zyzz <115093069+Prakash-zyzz@users.noreply.github.com>
2024-02-11 16:30:40 +0530

    Update server.js

--- server/config/env/production/server.js
+++ server/config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-    url: env("RENDER_EXTERNAL_URL"),
+    url: env("https://dressup-8pk8.onrender.com"),
     database: {
         connection: {
             filename:  "D:/data/public", // Adjust the path and database name as needed
